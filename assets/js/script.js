51e74f9e4fc65b911e865eec565c4ea8a89162be
MaxPaolo6600 <paolomax993@gmail.com>
2026-04-07 22:08:02 -0300

    bhtr

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,3 +25,25 @@ window.onload = function () {
         container.appendChild(card);
     });
 };
+
+async function carregarDados() {
+    try {
+        const infos = await fetch("../assets/js/dados.json");
+        const data = await infos.json();
+
+        const tela = document.getElementById("listaCorridas");
+        data.events.forEach(event => {
+            const div = document.createElement("div");
+            div.classList.add("cardInfo");
+            div.innerHTML = `
+                <h1 class="cardText1">${event.round} ${event.name}</h1>
+                <h3 class="cardText2">${event.location}</h3>
+                <h3 class="cardText2">Vencedor: <span>${event.winner.driver}</span> da equipe <span>${event.winner.team}</span></h3>
+            `;
+            tela.appendChild(div);
+        });
+    } catch (error) {
+        console.log("deu ruim");
+    }
+}
+carregarDados();
